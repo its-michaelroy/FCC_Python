ff635d891d485bff8caa74362ae93093899a905c
its-michaelroy <michaelr.social@skiff.com>
2024-01-20 18:06:35 -0300

    good stopping point with random() module

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,1 +1,15 @@
 #Learn to create password generator with REGEX
+import random
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+# Combine all characters
+all_characters = letters + digits + symbols
+
+print(all_characters)
+print(random())
